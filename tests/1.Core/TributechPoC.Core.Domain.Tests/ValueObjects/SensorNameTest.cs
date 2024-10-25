ad7a271571aac231bb02b852ec343a9f24e9bc06
Abbas Vosoughi <abbas769@hotmail.com>
2024-10-26 00:00:29 +0200

    feat: test units

---
+++ tests/1.Core/TributechPoC.Core.Domain.Tests/ValueObjects/SensorNameTest.cs
@@ -0,0 +1,25 @@
+﻿using Shouldly;
+using TributechPoC.Domain.Exceptions;
+using TributechPoC.Domain.ValueObjects;
+
+namespace TributechPoC.Core.Domain.Tests.ValueObjects
+{
+    [Trait("Category", "ValueObject")]
+    public class SensorNameTest
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("a")]        
+        public void Should_ThrowInvalidValueObjectStateException_When_InputIsInvalid(string inputData)
+        {
+            //Arrange
+
+            SensorName sensorName;
+            //Act
+
+            //Assert
+            Should.Throw<InvalidValueObjectStateException>(() => sensorName = new SensorName(inputData));
+        }
+    }
+}
